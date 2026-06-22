1d960303ca9591e21186e70525ffc82e9137ab7f
leandro291 <lenadrito1@gmail.com>
2026-06-22 11:23:13 -0500

    Agregar clase Database para la conexión a la base de datos

---
+++ config/database.php
@@ -0,0 +1,22 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $name = "inventario";
+    public mysqli $conn;
+
+    public function __construct() {
+        $this->conn = mysqli_connect(
+            $this->host,
+            $this->user,
+            $this->pass,
+            $this->name
+        );
+
+        if (!$this->conn) {
+            die("Error de conexión: " . mysqli_connect_error());
+        }
+    }
+}
+?>
